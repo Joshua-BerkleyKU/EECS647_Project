7a18084fdf9e99bea10672c9e8bf9215ba3162a2
Joshua-BerkleyKU <j708b537@ku.edu>
2022-04-29 13:02:34 -0500

    test

---
+++ backend.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$servername = "mysql.eecs.ku.edu";
+$username = "j708b537";
+$password = "Lais4suf";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+?>
