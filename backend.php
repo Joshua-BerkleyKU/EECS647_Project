072a46d76ec741300d21756ebc5aed0ca25ccd9a
Joshua-BerkleyKU <j708b537@ku.edu>
2022-04-29 13:08:28 -0500

    Update backend.php

--- backend.php
+++ backend.php
@@ -12,6 +12,10 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+// echo "Connected successfully";
+
+$query = "SELECT * FROM VISIT WHERE 'true'";
+echo "<br>Based on the user input, I created the following query: <br>".$query."<br><br>";
+$result = $conn->query($query);
 
 ?>
