0a9fc0af91fe91cb4a9dcd8e131f694b46a0dd83
Joshua-BerkleyKU <j708b537@ku.edu>
2022-05-02 22:20:44 -0500

    Update backend.php

--- backend.php
+++ backend.php
@@ -39,6 +39,7 @@ $conn = mysqli_connect($servername, $username, $password,$dbname);
 
 // Check connection
 if (!$conn) {
+  echo "<div> $username </div>";
   die("Connection failed: " . mysqli_connect_error());
 }
 // echo "Connected successfully";
